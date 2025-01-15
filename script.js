8795143f0880b544a91102dfa1495fdb548147f1
Khalidur34 <khalidur34@gmail.com>
2025-01-15 11:15:09 +1100

    new grid generates with hover effect with user input. Maximum 100 squares per axis

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 const gridContainer = document.querySelector(".grid");
 let gridButton = document.querySelector(".gridButton");
-let boxNum = 0;
+let boxes = document.querySelectorAll(".box");
+let defaultBox = 16;
+
 gridButton.onclick = function() {
-    boxNum = prompt("Enter number of boxes per side");
+    let currBox = prompt("Enter number of boxes per side");
+    if(currBox >= 100) {
+        currBox = 100;
+    }
+    clearGrid();
+    defaultBox = currBox;
+    createGrid(defaultBox);
 }
 
-for(i = 0; i < 16 ; i++) {
-    for(j = 0; j < 16 ; j++) {
+createGrid(defaultBox);
+
+function createGrid(boxNum) {
+    for(i = 0; i < boxNum*boxNum; i++) {
         let tempBox = createBox();
         gridContainer.appendChild(tempBox);
     }
 }
 
-function createBox() {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    return box;
+function clearGrid() {
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
 }
 
-let boxes = document.querySelectorAll(".box");
-boxes.forEach(element => {
-    element.onmouseover = function() {
+function createBox() {
+    let size = 500/defaultBox;
+    let box = document.createElement("div");
+    box.style.height = size + 'px';
+    box.style.width = size + 'px';
+    box.classList.add("box");
+    box.onmouseover = function() {
         this.style.backgroundColor = 'red';
     }
-});
+    return box;
+}
 
 
 
